17209fd5e70513a0c91f3e27f4f4662c658cc8ea
Vikas Gothi <125548865+vikasgothi48@users.noreply.github.com>
2023-04-16 11:42:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,19 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 export default function App() {
+    const [theme , setTheme]=
+          useState("light")
+    const func=()=>{
+        if(theme==="light"){
+            setTheme("dark")
+        }else{
+            setTheme("light")
+        }
+    }
     return (
         <div className="App">
-            <h1>Newton School</h1>
+        id={theme=="light"?"light":"dark"}>
+    <h1>Newton School</h1>
             <div>
                 <form>
                     <label>UserName</label>
@@ -15,7 +25,7 @@ export default function App() {
                 </form>
             </div>
             <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-            <input type="checkbox" id="switch" />
+            <input type="checkbox" id="switch" onChange={func} />
             <label for="switch" className="label">
                 Toggle
             </label>
